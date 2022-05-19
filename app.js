2847831421fe3d8056b1a71cd9216603f743c180
lwyers01 <lwyers01@qub.ac.uk>
2022-05-19 23:19:17 +0100

    added folder for redo - created html and css for calculator

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-//10 max
+//1000000000000000000000000000000
+
+
+
+
+const input = document.getElementsByClassName("input");
+console.log(parseInt((input[0].textContent)));
+
+
+const add = (a, b) => {
+    return a + b;
+};
+
+const subtract = (a, b) => {
+    return a - b;
+};
+
+const multiply = (a, b) => {
+    return a * b;
+};
+
+const divide = (a, b) => {
+    return a / b;
+};
+
