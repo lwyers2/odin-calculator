a0d4d4b8bcae4734a13927c25af09b62d7ca622e
lwyers01 <lwyers01@qub.ac.uk>
2022-05-20 18:01:12 +0100

    Started adding js. Need to update DOM for js

---
+++ app.js
@@ -0,0 +1,106 @@
+
+
+enterButtons();
+
+
+function enterButtons() {
+
+    let divide = document.querySelector("#divide");
+    divide.addEventListener("click", () => {
+        console.log("Divide");
+    });
+
+    let multiply = document.querySelector("#multiply");
+    multiply.addEventListener("click", () => {
+        console.log("Multiply");
+    });
+
+    let subtract = document.querySelector("#subtract");
+    subtract.addEventListener("click", () => {
+        console.log("subtract");
+    });
+
+    let add = document.querySelector("#add");
+    add.addEventListener("click", () => {
+        console.log("add");
+    });
+
+    let seven = document.querySelector("#seven");
+    seven.addEventListener("click", () => {
+        console.log("7");
+    });
+
+    let eight = document.querySelector("#8");
+    eight.addEventListener("click", () => {
+        console.log("eight");
+    });
+
+    let nine = document.querySelector("#9");
+    nine.addEventListener("click", () => {
+        console.log("nine");
+    });
+
+    let c = document.querySelector("#c");
+    c.addEventListener("click", () => {
+        console.log("Clear");
+    });
+
+    let four = document.querySelector("#4");
+    four.addEventListener("click", () => {
+        console.log("four");
+    });
+
+    let five = document.querySelector("#5");
+    five.addEventListener("click", () => {
+        console.log("five");
+    });
+
+    let six = document.querySelector("#6");
+    six.addEventListener("click", () => {
+        console.log("six");
+    });
+
+    let ce = document.querySelector("#ce");
+    ce.addEventListener("click", () => {
+        console.log("ce");
+    });
+
+    let one = document.querySelector("#1");
+    one.addEventListener("click", () => {
+        console.log("one");
+    });
+
+    let two = document.querySelector("#2");
+    two.addEventListener("click", () => {
+        console.log("two");
+    });
+
+    let three = document.querySelector("#3");
+    three.addEventListener("click", () => {
+        console.log("three");
+    });
+
+    let del = document.querySelector("#delete");
+    del.addEventListener("click", () => {
+        console.log("delete");
+    });
+
+    let zero = document.querySelector("#0");
+    zero.addEventListener("click", () => {
+        console.log("zero");
+    });
+
+    let decimal = document.querySelector("#decimal");
+    decimal.addEventListener("click", () => {
+        console.log("decimal");
+    });
+
+    let equal = document.querySelector("#equal");
+    equal.addEventListener("click", () => {
+        console.log("equal");
+    });
+
+
+
+
+}
